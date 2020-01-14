850eba713ac66513df69581a0e49aacb8ac0fb78
kenshi kamata <kenshi.kamata@gmail.com>
2020-01-14 20:20:15 +0900

    create line function to get node's line

---
+++ line.go
@@ -0,0 +1,41 @@
+package templateutil
+
+import (
+	"fmt"
+	"strings"
+	"text/template/parse"
+)
+
+var (
+	NodeNilError = fmt.Errorf("node is nil")
+)
+
+func Line(root, target parse.Node) (int, error) {
+	if root == nil {
+		return 0, fmt.Errorf("root: %w", NodeNilError)
+	}
+	if target == nil {
+		return 0, fmt.Errorf("target: %w", NodeNilError)
+	}
+	line := 0
+	Inspect(root, func(n parse.Node) bool {
+		if n == nil {
+			return false
+		}
+		if target.Position() > n.Position() {
+			return true
+		}
+		if tn, ok := n.(*parse.TextNode); ok {
+			if strings.Contains(string(tn.Text), "\n") {
+				line++
+			}
+		}
+		if tn, ok := n.(*parse.StringNode); ok {
+			if strings.Contains(string(tn.Text), "\n") {
+				line++
+			}
+		}
+		return true
+	})
+	return line, nil
+}
---
+++ line_test.go
@@ -0,0 +1,61 @@
+package templateutil
+
+import (
+	"errors"
+	"html/template"
+	"testing"
+)
+
+func TestLine(t *testing.T) {
+	tpl := `this is {{ println "template" }}
+    hogehuga{{ println }}
+`
+	tmpl, err := template.New("Ident").Parse(tpl)
+	if err != nil {
+		t.Fatalf("parsing template %s", err)
+	}
+	l, err := Line(tmpl.Tree.Root, tmpl.Tree.Root.Nodes[len(tmpl.Tree.Root.Nodes)-2])
+	if err != nil {
+		t.Fatal(err)
+	}
+	expected := 1
+	if l != expected {
+		t.Errorf("result=%d != expected=%d", l, expected)
+	}
+}
+
+func TestLine_NoLine(t *testing.T) {
+	tpl := `this is {{ println "template" }}hogehuga{{ println }}`
+	tmpl, err := template.New("Ident").Parse(tpl)
+	if err != nil {
+		t.Fatalf("parsing template %s", err)
+	}
+	l, err := Line(tmpl.Tree.Root, tmpl.Tree.Root.Nodes[len(tmpl.Tree.Root.Nodes)-2])
+	if err != nil {
+		t.Fatal(err)
+	}
+	expected := 0
+	if l != expected {
+		t.Errorf("result=%d != expected=%d", l, expected)
+	}
+}
+
+func TestLine_NoText(t *testing.T) {
+	tpl := ``
+	tmpl, err := template.New("Ident").Parse(tpl)
+	if err != nil {
+		t.Fatalf("parsing template %s", err)
+	}
+	_, err = Line(tmpl.Tree.Root, nil)
+	t.Log(err)
+	if err == nil {
+		t.Fatal("expected error but no")
+	}
+	originalErr := errors.Unwrap(err)
+	if originalErr == nil {
+		t.Fatal("expected error but no")
+	}
+	if !errors.Is(originalErr, NodeNilError) {
+		t.Fatal("unexpected error: ", err)
+	}
+}
